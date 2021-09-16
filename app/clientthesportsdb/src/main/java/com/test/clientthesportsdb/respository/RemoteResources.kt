e9341e356752ee408a54530ae9a7788fcef32f61
chaouki <chawachi.chawki.ing@gmail.com>
2021-09-16 14:59:54 +0200

    add remote Ressources

---
+++ app/clientthesportsdb/src/main/java/com/test/clientthesportsdb/respository/RemoteResources.kt
@@ -0,0 +1,9 @@
+package com.test.clientthesportsdb.respository
+
+import com.test.clientthesportsdb.model.Leagues
+import com.test.clientthesportsdb.model.Teams
+
+interface RemoteResources {
+        suspend fun requestAllLeagues() : Resource<Leagues>
+        suspend fun requestAllTeamsByLeagues() : Resource<Teams>
+}
