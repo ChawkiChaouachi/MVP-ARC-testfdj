d194f193bf27edeea74aab6e3330e918790e30eb
chaouki <chawachi.chawki.ing@gmail.com>
2021-09-16 19:46:19 +0200

    use default adapter for autocomplete

--- app/src/main/java/com/test/fdj/ui/leagues/LeagueActivity.kt
+++ app/src/main/java/com/test/fdj/ui/leagues/LeagueActivity.kt
@@ -3,19 +3,25 @@ package com.test.fdj.ui.leagues
 import android.os.Bundle
 import com.test.clientthesportsdb.model.Leagues
 import com.test.fdj.R
+import com.test.fdj.modelapp.sLeague
 import com.test.fdj.ui.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+
 
 @AndroidEntryPoint
 class LeagueActivity : BaseActivity() ,LeagueContract.View{
 
      @Inject
      lateinit var presenterLeague: LeaguesPresenter
+     lateinit var localLeagues : List<sLeague>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
 
     }
 
@@ -24,8 +30,14 @@ class LeagueActivity : BaseActivity() ,LeagueContract.View{
         super.presenter = presenterLeague
     }
 
-    override fun initializeLeaguesList(leagues: Leagues) {
+    override fun initializeLeaguesList(leagues: List<sLeague>) {
+        localLeagues=leagues
+        val adapter: ArrayAdapter<String> = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_dropdown_item_1line, leagues.map { it.strLeague }
 
+        )
+        findViewById<AutoCompleteTextView>(R.id.leagues_autocomplete).setAdapter(adapter)
     }
 
     override fun onResume() {
